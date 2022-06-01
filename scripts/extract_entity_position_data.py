7e304bb59b26b82457ffa68698c4d1e5ee9235c3
JaceCear <11230293+JaceCear@users.noreply.github.com>
2022-06-01 20:21:22 +0200

    Add non-finished script for merging

--- scripts/extract_entity_position_data.py
+++ scripts/extract_entity_position_data.py
@@ -22,12 +22,14 @@ class Entity(IntEnum):
 
 #tables = [0x000BAF8C, 0x000BAFD4, 0x000BB01C, 0x000BAF44] # SA1 (PAL)
 tables = [0x000D4C84, 0x000D4D0C, 0x000D4D94, 0x000D4BFC] # SA2 (PAL, NTSC)
-#tables = [0x000D4C84, 0x000D4D0C, 0x000D4D94, 0x000D4BFC] # SA2 (PAL, NTSC)
 #tables = [0x000D1224, 0x000D1348, 0x000D146C, 0x000CEF58] # SA3 (PAL)
 #tables = [0x000D0E9C, 0x000D0FC0, 0x000D10E4, 0x000CEBD0] # SA3 (NTSC)
 #tables = [0x000D1218, 0x000D133C, 0x000D1460, 0x000CEF4C] # SA3 (JP)
 
-table_lengths = [0x88//4, 0x88//4, 0x88//4, 0x88//4] # SA2
+#total_stage_count_sa1 = 18
+total_stage_count_sa2 = 34
+#total_stage_count_sa3 = 73
+tables_length = total_stage_count_sa2 # SA2
 
 final_position_entry_end = 0x80AED70 # SA2 (PAL, NTSC)
 
@@ -40,36 +42,32 @@ def file_addr(addr):
     return addr
 
 # TODO
-def print_asm_file(all_addresses, file_name_format):
+def print_asm_file(all_addresses, duplicates, export_path):
     print("\t.section .rodata")
-    print(file_name_format)
+    print(export_path)
     for i in range(len(all_addresses)):
         entity_index = all_addresses[i][0]
         stage_index = all_addresses[i][1]
         address = all_addresses[i][2]
 
-        if (i > 0):
-            prev_address = all_addresses[i-1][2]
-
-            if(prev_address == address):
-                continue
-
+        
 
         entity_type = Entity(entity_index).name.lower()
         label_str = f"stage_{stage_index}_{entity_type}"
         print(f"\t.global {label_str}")
         print(f"{label_str}: @ {hex(address)}")
-        print(f"\t.incbin \"data/entity_positions/{label_str}.bin.rl\"")
+        print(f"\t.incbin \"{export_path}/{label_str}.bin.rl\"")
         print()
 
 with open('baserom.gba', 'rb') as rom:
     all_addresses = []
+    duplicates = []
 
     # Fill all_addresses with the addresses in the tables
     for entity_index in range(len(Entity)):
         rom.seek(tables[entity_index])
 
-        for stage_index in range(table_lengths[entity_index]):
+        for stage_index in range(tables_length):
             address = read_u32(rom)
 
             if (address > 0):
@@ -122,9 +120,24 @@ with open('baserom.gba', 'rb') as rom:
         os.mkdir(export_directory)
 
 
+    ## Find out duplicates ##
+    # (make i start at 1 instead of 0)
+    all_range = list(range(len(all_addresses)))
+    del(all_range[0])
+
+    for i in all_range:
+        last_address = all_addresses[i-1][2]
+        current_address = all_addresses[i][2]
+        if(current_address and current_address == last_address):
+            duplicates.append(current_address)
+
+    """
     print("Extracting and decompressing data...")
     for entity_index in range(len(Entity)):
-        for stage_index in range(table_lengths[entity_index]):
+        for stage_index in range(tables_length):
+            addresses_index = entity_index*tables_length+stage_index
+            # FIXME:
+            #Using stage_index here is wrong
             start_addr = file_addr(all_addresses[stage_index][2])
 
             length = all_addresses[stage_index][3]
@@ -134,9 +147,13 @@ with open('baserom.gba', 'rb') as rom:
 
             # Don't re-export duplicates
             if (stage_index > 0):
+            # FIXME:
+            #Using stage_index here is wrong
                 prev_address = file_addr(all_addresses[stage_index-1][2])
 
-                if(prev_address == start_addr):
+                if(start_addr and prev_address == start_addr):
+                    if not start_addr in duplicates:
+                        duplicates.append((stage_index, start_addr))
                     continue
 
             # Generate file names
@@ -157,8 +174,8 @@ with open('baserom.gba', 'rb') as rom:
 
             # Decompress the exported files
             os.system(f"tools{slash}gbagfx{slash}gbagfx {position_file_compressed} {position_file_uncompressed}")
-
+    """
     print("Done!")
     print()
-    print(os.name)
-    #print_asm_file(all_addresses, export_directory)
+    print()
+    #print_asm_file(all_addresses, duplicates, export_directory)
